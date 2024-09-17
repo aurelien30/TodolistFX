cf985f09b7183928f65b82a252215979b3f250ab
aurelien30 <151914448+aurelien30@users.noreply.github.com>
2024-09-17 19:45:46 +0200

    Update StartApplication.java

--- src/main/java/appli/StartApplication.java
+++ src/main/java/appli/StartApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class StartApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("loginView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("accueil/loginView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
